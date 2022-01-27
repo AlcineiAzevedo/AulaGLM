e04f772d5de8f11d8043f40cf79ab751418175be
AlcineiAzevedo <65502006+AlcineiAzevedo@users.noreply.github.com>
2022-01-27 10:39:01 -0300

    Add files via upload

---
+++ Rotina2.R
@@ -0,0 +1,18 @@
+remove(list=ls())
+
+install.packages("ExpGLM")
+
+#  Instalacao do pacote pelo github se nao for possivel instalar pelo comando anterior.
+#   if(!requireNamespace("devtools", quietly = TRUE))install.packages("devtools")
+#   devtools::install_github("AlcineiAzevedo/ExpGLM")
+
+
+library(ExpGLM)
+?ExpGLM
+?BinomialReg
+
+setwd_script()
+ D1=read.table("Dados1.txt",h=TRUE)
+ Res=BinomialReg(data=D1,TwoWay=FALSE,Family="probit")
+ Res=BinomialReg(data=D1,TwoWay=FALSE,Family="logit")
+ 
